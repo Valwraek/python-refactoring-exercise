019953f20462b12f6ec6e244c670f8ac69fa148d
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-09 18:45:20 +0100

    refactor(src): :recycle: refactor function ones of yatzy1.py

--- yatzy-refactoring-kata/src/yatzy1.py
+++ yatzy-refactoring-kata/src/yatzy1.py
@@ -21,20 +21,12 @@ class Yatzy:
         return Yatzy.FIFTY if dice.count(dice[0]) == 5 else Yatzy.ZERO
 
     @staticmethod
-    def ones(d1, d2, d3, d4, d5):
-        sum = 0
-        if (d1 == 1):
-            sum += 1
-        if (d2 == 1):
-            sum += 1
-        if (d3 == 1):
-            sum += 1
-        if (d4 == 1):
-            sum += 1
-        if (d5 == 1):
-            sum += 1
-
-        return sum
+    def ones(*dice):
+        '''
+        1. The function can receive multiple parameters.
+        2. The method ".count()" to simplify an overly complex function.
+        '''
+        return dice.count(1)
 
     @staticmethod
     def twos(d1, d2, d3, d4, d5):
