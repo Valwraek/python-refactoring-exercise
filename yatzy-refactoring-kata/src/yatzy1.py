56c4c1b4f0dca70775cb58ae64777c616b86bde2
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-09 17:46:28 +0100

    refactor(src): :recycle: refactor function chance of yatzy1.py

--- yatzy-refactoring-kata/src/yatzy1.py
+++ yatzy-refactoring-kata/src/yatzy1.py
@@ -1,14 +1,12 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dice):
+        '''
+        1. The function can receive multiple parameter.
+        2. Use sum() function to calculate the sum of the dice.  
+        '''
+        return sum(dice)
 
     @staticmethod
     def yatzy(dice):
