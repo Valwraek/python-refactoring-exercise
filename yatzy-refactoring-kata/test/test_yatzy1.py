8b7f608ec7a12c21939231c597a6d1533df89944
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-10 13:15:19 +0100

    test(test): :recycle: refactor function test_three_of_a_kind of test_yatzy1.py
    Refactored the test test_three_of_a_kind.

--- yatzy-refactoring-kata/test/test_yatzy1.py
+++ yatzy-refactoring-kata/test/test_yatzy1.py
@@ -107,9 +107,10 @@ def test_two_pair():
 
 
 def test_three_of_a_kind():
-    assert 9 == Yatzy().three_of_a_kind(3, 3, 3, 4, 5)
-    assert 15 == Yatzy().three_of_a_kind(5, 3, 5, 4, 5)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
+    assert 15 == Yatzy.three_of_a_kind(5, 3, 5, 4, 5)
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
+    assert 0 == Yatzy.three_of_a_kind(1, 2, 3, 4, 5)
 
 
 def test_four_of_a_knd():
