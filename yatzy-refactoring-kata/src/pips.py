c84f266cba4f52b6cb6d5e0ada7e71dd3b4f8f0b
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-15 18:28:14 +0100

    feat(src): :sparkles: add diferents classmethod for module pips.py

--- yatzy-refactoring-kata/src/pips.py
+++ yatzy-refactoring-kata/src/pips.py
@@ -9,3 +9,20 @@ class Pips(Enum):
     FOUR = 4
     FIVE = 5
     SIX = 6
+    
+    @classmethod
+    def values(cls):
+        return set(member.value for member in cls)
+    
+    @classmethod
+    def names(cls):
+        return [member.name for member in cls]
+    
+    @classmethod
+    def items(cls):
+        return [(member.name, member.value) for member in cls]
+
+if __name__ == '__main__':
+    print(Pips.values())
+    print(Pips.names())
+    print(Pips.items())
