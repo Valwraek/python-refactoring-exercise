fe8e3b8a64772f2a090fe187785bf2bca56bc3bd
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-15 18:33:53 +0100

    refactor(test): :recycle: refactor function of module test_yatzy1.py

--- yatzy-refactoring-kata/test/test_yatzy1.py
+++ yatzy-refactoring-kata/test/test_yatzy1.py
@@ -144,6 +144,7 @@ def test_large_straight():
     assert 20 == Yatzy.large_straight(6, 2, 3, 4, 5)
     assert 20 == Yatzy.large_straight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.large_straight(1, 2, 2, 4, 5)
+    assert 0 == Yatzy.large_straight(2, 2, 6, 4, 5)
 
 
 def test_full_house():
